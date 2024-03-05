4680e2945f8bfdb4467e0e51762293ff45b106d7
GRK-Gith <121422573+GRK-Gith@users.noreply.github.com>
2024-03-05 21:14:07 +0800

    Update GitTest.java

--- src/main/java/com/guigu/GitTest.java
+++ src/main/java/com/guigu/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("master1 merge");
         System.out.println("hot-fix1 merge");
         System.out.println("push first github");
+         System.out.println("pull first idea");
     }
 }
